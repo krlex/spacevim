7a5aa74797fca6dbd92ef0417a024a777642c885
Goran Mekić <meka@tilda.center>
2018-02-20 12:10:52 +0100

    Proper install

--- bin/init.sh
+++ bin/init.sh
@@ -2,6 +2,9 @@
 
 VIM_ROOT_DIR="$(readlink -f $(dirname $0)/..)"
 
-rm -rf ~/.spacevim
+cd
+rm -rf .spacevim
 ln -s "${VIM_ROOT_DIR}/spacevim" ~/.spacevim
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/liuchengxu/space-vim/master/install.sh)"
+git clone https://github.com/liuchengxu/space-vim .space-vim
+cd .space-vim
+make vim
